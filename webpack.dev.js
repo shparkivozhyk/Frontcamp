0715ba3f5759deb56a28bff61d6e79ab32526646
Maryna Kavetskaya <m.kovetskaya@gmail.com>
2017-12-12 21:50:05 +0300

    devServer config added

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,9 +1,19 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
+const webpack = require('webpack');
 
 module.exports = merge(common, {
     devtool: 'inline-source-map',
+    plugins: [
+        new webpack.NamedModulesPlugin(),
+        new webpack.HotModuleReplacementPlugin()
+    ],
     devServer: {
-        contentBase: './dist'
+        contentBase: './dist',
+        host: '127.0.0.1',
+        port: 3000,
+        compress: true,
+        hot: true,
+        open: true
     }
 });
